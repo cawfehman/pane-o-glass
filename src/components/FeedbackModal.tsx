c451e8be4765b723f9018dd9e938260576f43cfd
Rivera <rivera-robert@cooperhealth.edu>
2026-07-14 11:45:21 -0400

    fix: import useEffect in FeedbackModal to fix client side crash

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 export default function FeedbackModal() {
